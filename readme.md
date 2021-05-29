2243ab0c362f9fd2641c47895ea9fff8a2568aca
Divyansh Kumar <divyanshkrit@gmail.com>
2021-05-30 05:20:52 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,14 @@
+# JS Projects
+## Quote Generator
+
+
+Quote generator, as the name says generates random quotes with their author names and display them in a simple and clean manner.
+
+- Tweet button, you can directly tweet the quote you like.
+- Next quote button with a clean loader
+- Completely optimised so that minimal resources(network and storage) will be used.
+- Quotes are fetched with the help of Forismatic API
+
+Make sure ton gain temporary access from CORS.
+
+- [ACCESS](https://cors-anywhere.herokuapp.com/corsdemo)
---
+++ script.js
@@ -0,0 +1,100 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+const oldQuoteButton = document.getElementById('old-quote');
+const loader = document.getElementById('loader');
+let errCount = 0;
+
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function done(){
+    if(!loader.hidden){
+        quoteContainer.hidden=false;
+        loader.hidden=true;
+    }
+}
+
+// let currentQuote = '';
+// let currentAuthor = '';
+// Fetching from API http://api.forismatic.com/api/1.0/
+
+async function getQuote(){
+    loading();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        
+        console.log(data);
+        // author data
+        
+        if(data.quoteAuthor.length===0){
+            authorText.innerText = '-unknown';
+        }
+        else{
+            authorText.innerText = '- '+ data.quoteAuthor;
+        }
+        
+        // quote data
+        if(data.quoteText.length>100){
+            quoteText.classList.add('long-quote');
+        }
+        else{
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText;
+        
+        //stop loader and show quote
+        done();
+    } catch(error){
+            console.log('OMG ', error);
+            errCount++;
+            if(errCount>10){
+                alert("Oops! Techincal glitch from API call, please refresh. Please gain CORS access. Check the footer !")
+            }
+            else{
+                getQuote();
+            }
+        }
+}
+
+currentAuthor = authorText.innerText;
+currentQuote = quoteText.innerText;
+
+// function getQuoteP(){ 
+//         authorText.innerText = currentAuthor;
+//         quoteText.innerText = currentQuote;
+// }
+
+
+
+
+// Tweet button functionality - https://twitter.com/intent/tweet
+
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerHTML;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+
+}
+
+//button event listener
+
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
+
+
+
+// on webpoage load
+
+getQuote();
