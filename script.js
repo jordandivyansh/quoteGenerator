e7d6bf3d36bf6298be173dfbdd45a2d7e523df32
Divyansh Kumar <divyanshkrit@gmail.com>
2021-08-06 02:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function done(){
 
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const proxyUrl = 'https://secure-tundra-47200.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);
